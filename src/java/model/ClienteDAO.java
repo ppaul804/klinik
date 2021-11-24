8e324947d5407d210fcb22fc95268fa0fb116b73
André Cordeiro <93953859+AndreCordeir0@users.noreply.github.com>
2021-11-24 19:55:46 -0300

    Deem uma olhada

--- src/java/model/ClienteDAO.java
+++ src/java/model/ClienteDAO.java
@@ -1,1 +1,41 @@
+package model;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+
+public class ClienteDAO extends DataBaseDAO {
+    
+    public ClienteDAO() throws Exception {}
+    public ArrayList<Cliente> getLista() throws Exception{
+        
+        ArrayList<Cliente> lista = new ArrayList<Cliente>();
+        String sql = "SELECT * FROM cliente";
+        
+        this.conectar();
+        PreparedStatement pstm = conn.prepareStatement(sql);
+        ResultSet rs = pstm.executeQuery();
+        
+        while (rs.next()) {            
+            Cliente cliente = new Cliente();
+            cliente.setIdCliente(rs.getInt("cliente.idCliente"));
+  
+            cliente.setNome(rs.getString("cliente.NOME"));
+            cliente.setCpf(rs.getString("cliente.CPF"));
+            cliente.setRg(rs.getString("cliente.RG"));
+            cliente.setEmail(rs.getString("cliente.EMAIL"));
+            cliente.setTelefone(rs.getString("cliente.TELEFONE"));
+            cliente.setEndereco(rs.getString("cliente.ENDERECO"));
+            cliente.setComplemento(rs.getString("cliente.COMPLEMENTO"));
+            cliente.setCidade(rs.getString("cliente.CIDADE"));
+            cliente.setSexo(rs.getString("cliente.SEXO").charAt(0));
+            cliente.setData_de_nascimento(rs.getDate("cliente.DATA_DE_NASCIMENTO"));
+            lista.add(cliente);
+        }
+        
+        this.desconectar();
+        return lista;
+        
+    }
+}
